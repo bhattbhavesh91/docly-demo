2070523684da95e74cbd09ad7704cb4b43a5b484
Bhavesh Bhatt <bhattbhavesh91@gmail.com>
2020-11-27 17:56:30 +0530

    Add files via upload

---
+++ script_1.py
@@ -0,0 +1,9 @@
+def detect_problems(filename):
+	
+    data = numpy.loadtxt(fname=filename, delimiter=',')
+    if numpy.max(data, axis=0)[0] == 0 and numpy.max(data, axis=0)[20] == 20:
+        print('Suspicious looking maxima!')
+    elif numpy.sum(numpy.min(data, axis=0)) == 0:
+        print('Minima add up to zero!')
+    else:
+        print('Seems OK!')
---
+++ script_2.py
@@ -0,0 +1,7 @@
+def calculate_critical_value(size : int, alpha : float) -> float:
+    t_dist = stats.t.ppf(1 - alpha / (2 * size), size - 2)
+    numerator = (size - 1) * np.sqrt(np.square(t_dist))
+    denominator = np.sqrt(size) * np.sqrt(size - 2 + np.square(t_dist))
+    critical_value = numerator / denominator
+    print("Grubbs Critical Value: {}".format(critical_value))
+    return critical_value
